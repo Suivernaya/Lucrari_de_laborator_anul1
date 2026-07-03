e3ac39c8f4b9d1203e54dc64e8a6791f84a9e2ed
Suivernaya <sovic.anaga@gmail.com>
2026-07-03 20:15:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаборатор_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Problema 1//
+            /*
+            int a, b;
+            Console.WriteLine("introduceti 2 numere untregi");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"ati introdus {a} si {b}");
+            */
+            // Problema 2//
+            /*
+            int a, b;
+            Console.WriteLine("introduceti 2 numere untregi");
+            a= int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            int suma = a + b;
+            int diferenta = a - b;
+            int produsul = a * b;
+            int rest = a % b;
+            int cat = a / b;
+            Console.WriteLine($"suma {suma}"); 
+            Console.WriteLine($"diferenta {diferenta}");
+            Console.WriteLine($"produsul {produsul}");
+            Console.WriteLine($"restul impartirii {rest}");
+            Console.WriteLine($"catul impartirii {cat}");
+            */
+            //problema 3//
+            /*
+            int baieti, fete;
+            Console.WriteLine("introduceti numarul de fete");
+            fete = int.Parse(Console.ReadLine());
+            baieti = fete + 10;
+            Console.WriteLine(baieti);
+            */
+            //Problema 4//
+            /*
+            int sc2, sc3, suma;
+            Console.WriteLine("nr de elevi din a 2 scoala: ");
+            sc2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("nr de elevi din a 3 scoala: ");
+            sc3 = int.Parse(Console.ReadLine());
+            suma = sc2 + sc3 + 7;
+            Console.WriteLine($"suma = {suma}");
+           */
+            Console.ReadKey();
+        }
+    }
+}
